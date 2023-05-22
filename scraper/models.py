d5a6c5fe0fff401d717579ee3ec0a3cba32b9a79
Marek <marekdrag@gmail.com>
2023-05-22 15:42:16 +0200

    Modified Models and Tasks for Scraper App
    Updated the models in the scraper app to reflect the changes. Created a new models.py file with the following modifications:
    
    Added a new model "City" to represent a cinema, which is associated with a Cinema and ScraperData.
    Added a "category" field to the Film model.
    Modified the Showtime model to include a "date" field and changed the "city" foreign key to refer to the City model.
    Added a ScraperData model to store scraped data for a cinema, including the date of scraping.
    Adjusted the tasks.py file to accommodate the model changes.
    
    Old models.py:
    Removed the "address" field from the Cinema model.
    
    These changes ensure that the scraper retrieves movie titles, showtimes, and reservation links for each showtime from any cinemas.

--- scraper/models.py
+++ scraper/models.py
@@ -2,10 +2,16 @@ from django.db import models
 
 
 class Cinema(models.Model):
+    name = models.CharField(max_length=255)
+    url = models.URLField(max_length=255)
+
+
+class City(models.Model):
     """Represents a cinema."""
     name = models.CharField(max_length=255)
-    address = models.CharField(max_length=255, null=True)  # Make it optional as it can be empty
     url = models.URLField(max_length=255)
+    cinema = models.ForeignKey('Cinema', on_delete=models.CASCADE, related_name='city')
+    data = models.ForeignKey('ScraperData', on_delete=models.CASCADE, related_name='data')
 
 
 class Film(models.Model):
@@ -14,19 +20,17 @@ class Film(models.Model):
     category = models.CharField(max_length=255, null=True)
     description = models.TextField(default="")
     image_url = models.URLField()
-    cinema = models.ForeignKey('Cinema', on_delete=models.CASCADE, related_name='films')
+    city = models.ForeignKey('City', on_delete=models.CASCADE, related_name='films')
 
 
 class Showtime(models.Model):
     """Represents a film's showtime at a cinema."""
     film = models.ForeignKey('Film', on_delete=models.CASCADE, related_name='showtimes')
-    cinema = models.ForeignKey('Cinema', on_delete=models.CASCADE, related_name='showtimes')
-    date = models.DateField()
+    city = models.ForeignKey('City', on_delete=models.CASCADE, related_name='showtimes')
     time = models.TimeField(default="")
     booking_link = models.URLField(default="")
 
 
 class ScraperData(models.Model):
     """Represents scraped data for a cinema."""
-    cinema = models.ForeignKey('Cinema', on_delete=models.CASCADE, related_name='scraper_data')
-    scraped_date = models.DateField(auto_now_add=True)
+    date = models.DateField()
--- scraper/tasks.py
+++ scraper/tasks.py
@@ -1,6 +1,6 @@
 from datetime import datetime
-from .models import Film, Cinema, Showtime, ScraperData
-from .scraper import get_movie_info
+from .models import Film, Cinema, Showtime, ScraperData, City
+from .scraper import get_movie_info_from_multikino
 from celery import shared_task
 
 
@@ -11,24 +11,34 @@ def scrape_and_store_data():
     """
     # Here, add the list of cities you want to scrape
     cities = ["kielce", "gdansk", "krakow", "lublin"]
+    cinema_name = 'multikino'
+
+    # Get today's date and fetch movie data
+    today = datetime.today().strftime('%Y-%m-%d')
+
+    # Save the scraping date
+    scraper_data = ScraperData.objects.create(date=today)
+
+    # Search for the cinema, if it doesn't exist - create a new one
+    cinema, created = Cinema.objects.get_or_create(name=cinema_name)
+    if created:
+        cinema.name = cinema_name
+        cinema.url = f"https://multikino.pl/"
+        cinema.save()
 
     for city in cities:
-        # Search for the cinema, if it doesn't exist - create a new one
-        cinema, created = Cinema.objects.get_or_create(name=city)
+        # Search for the cinema and city, if it doesn't exist - create a new one
+        cinema_in_city, created = City.objects.get_or_create(name=city, cinema=cinema, data=scraper_data)
         if created:
-            cinema.url = f"https://multikino.pl/repertuar/{city}/teraz-gramy"
-            cinema.save()
+            cinema_in_city.name = city
+            cinema_in_city.url = f"https://multikino.pl/{city}"
+            cinema_in_city.save()
 
-        # Save the scraping date
-        scraper_data = ScraperData.objects.create(cinema=cinema)
-
-        # Get today's date and fetch movie data
-        today = datetime.today().strftime('%Y-%m-%d')
-        movie_info_list = get_movie_info(city, today)
+        movie_info_list = get_movie_info_from_multikino(city, today)
 
         for movie_info in movie_info_list:
             # Create a new film if it doesn't exist
-            film, created = Film.objects.get_or_create(title=movie_info['title'], cinema=cinema)
+            film, created = Film.objects.get_or_create(title=movie_info['title'], city=cinema_in_city)
 
             if created:
                 film.description = movie_info['description']
@@ -43,8 +53,7 @@ def scrape_and_store_data():
                 # Create a new Showtime if it doesn't exist
                 showtime, created = Showtime.objects.get_or_create(
                     film=film,
-                    cinema=cinema,
-                    date=today,
+                    city=cinema_in_city,
                     time=show_time,
                     booking_link=show_info['booking_link']
                 )
