dc5e074a968400a65cd75748fc2d075e065b9aff
Marek <marekdrag@gmail.com>
2023-06-03 00:47:08 +0200

    Extend 'tests_scraper.py' with additional test cases
    This commit expands the 'tests_scraper.py' file with the addition of several test cases, improving the robustness of the test suite.
    
    The following test classes were added:
    
    1. `TestWebDriverManager`: This class tests the WebDriverManager class. A test case has been added to verify the creation of a Chrome WebDriver object.
    
    2. `TestBaseMovieScraper`: A test class for the BaseMovieScraper class. It includes a test case that checks whether 'get_movie_info' raises a NotImplementedError.
    
    3. `TestMultikinoScraper`: A class testing the MultikinoScraper class. It contains a test case that verifies if the 'get_movie_info' method returns correct data.
    
    4. `TestHeliosScraper`: This test class is for the HeliosScraper class. It includes a test case to verify that 'get_movie_info' returns correct data.
    
    All these test cases ensure that the scraper classes are functioning as expected, raising the overall confidence in the application's performance and functionality.

--- scraper/tests/tests_scraper.py
+++ scraper/tests/tests_scraper.py
@@ -1,10 +1,11 @@
-import unittest
+from unittest import TestCase
 from unittest.mock import patch, MagicMock
-from selenium.common import WebDriverException
-from scraper.scraper import MultikinoScraper, HeliosScraper
+from selenium.common.exceptions import WebDriverException
+from selenium.webdriver.chrome.service import Service
+from scraper.scraper import MultikinoScraper, HeliosScraper, WebDriverManager, BaseMovieScraper
 
 
-class TestLogging(unittest.TestCase):
+class TestLogging(TestCase):
     """
     Unit tests for exception logging in the scraper module.
     """
@@ -31,7 +32,7 @@ class TestLogging(unittest.TestCase):
         mock_logging_error.assert_called_with('Error occurred: Message: Test exception\n')
 
 
-class TestHeliosScraperLogging(unittest.TestCase):
+class TestHeliosScraperLogging(TestCase):
     """
     Unit tests for exception logging in the HeliosScraper class.
     """
@@ -56,3 +57,98 @@ class TestHeliosScraperLogging(unittest.TestCase):
 
         # Assert
         mock_logging_error.assert_called_with('Error occurred: Message: Test exception\n')
+
+
+class TestWebDriverManager(TestCase):
+    """
+    Test class for the WebDriverManager class.
+    """
+
+    @patch("selenium.webdriver.Chrome")
+    @patch("selenium.webdriver.ChromeOptions")
+    def test_get_chrome_driver(self, mock_options, mock_driver):
+        """
+        Test case for creating a Chrome WebDriver object.
+        """
+        with WebDriverManager().get_chrome_driver() as driver:
+            self.assertEqual(driver, mock_driver.return_value)
+
+        mock_options.assert_called()
+
+        # Ensure Chrome was called with an instance of Service
+        args, kwargs = mock_driver.call_args
+        self.assertIsInstance(kwargs['service'], Service)
+        self.assertEqual(kwargs['options'], mock_options.return_value)
+
+
+class TestBaseMovieScraper(TestCase):
+    """
+    Test class for the BaseMovieScraper class.
+    """
+    def test_get_movie_info_not_implemented(self):
+        """
+        Test case to check that get_movie_info raises NotImplementedError.
+        """
+        class FakeScraper(BaseMovieScraper):
+            pass
+
+        with self.assertRaises(TypeError):
+            FakeScraper().get_movie_info("city", "date")
+
+
+class TestMultikinoScraper(TestCase):
+    """
+    Test class for the MultikinoScraper class.
+    """
+    @patch('scraper.scraper.WebDriverManager.get_chrome_driver')
+    def test_get_movie_info(self, mock_get_chrome_driver):
+        """
+        Test case to check that get_movie_info returns correct data.
+        """
+        # Setup mock driver
+        mock_driver = MagicMock()
+        mock_driver.get.return_value = None
+        mock_get_chrome_driver.return_value.__enter__.return_value = mock_driver
+
+        # Setup MultikinoScraper
+        scraper = MultikinoScraper()
+
+        # Test get_movie_info
+        data = scraper.get_movie_info("city", "date")
+
+        expected_keys = {"title", "category", "description", "image", "show_info"}
+        assert all(expected_keys.issubset(item.keys()) for item in data)
+        assert all(isinstance(item['show_info'], list) for item in data)
+        assert all(
+            all(isinstance(show, dict) and 'hour' in show and 'booking_link' in show for show in item['show_info']) for
+            item in data)
+        assert all(isinstance(item[key], str) for item in data for key in ["title", "category", "description", "image"])
+
+
+class TestHeliosScraper(TestCase):
+    """
+    Test class for the HeliosScraper class.
+    """
+    @patch('scraper.scraper.WebDriverManager.get_chrome_driver')
+    def test_get_movie_info(self, mock_get_chrome_driver):
+        """
+        Test case to check that get_movie_info returns correct data.
+        """
+        # Setup mock driver
+        mock_driver = MagicMock()
+        mock_driver.get.return_value = None
+        mock_get_chrome_driver.return_value.__enter__.return_value = mock_driver
+
+        # Setup HeliosScraper
+        scraper = HeliosScraper()
+
+        # Test get_movie_info
+        data = scraper.get_movie_info("city", 1, 1)
+
+        expected_keys = {"title", "show_info"}
+        assert all(expected_keys.issubset(item.keys()) for item in data)
+        assert all(isinstance(item['show_info'], list) for item in data)
+        assert all(
+            all(isinstance(show, dict) and 'hour' in show and 'booking_link' in show for show in item['show_info']) for
+            item in data)
+        assert all(isinstance(item[key], str) for item in data for key in ["title"])
