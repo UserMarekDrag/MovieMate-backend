725f84162d4fb3bf811f71c887effcb4d59957f6
Marek <marekdrag@gmail.com>
2023-06-02 15:28:02 +0200

    Added 'tests' folder and reorganize tests in the Scraper APP
    This commit includes the following changes:
    
    1. A new 'tests' folder has been added to the 'scraper' application.
    2. Existing tests for 'scraper.py' have been moved to the new 'tests' folder.
    3. New unit tests have been added for 'models.py'. These tests check the accuracy of model creation.
    4. All the tests have been run and pass successfully.
    
    These changes are aimed at improving the structure and organization of our tests, making it easier to locate and understand what each test is doing. The new tests for 'models.py' will help ensure the correctness of our models.

---
+++ scraper/tests/tests_models.py
@@ -0,0 +1,48 @@
+from django.test import TestCase
+from scraper.models import Cinema, Movie, Show
+
+
+class CinemaModelTest(TestCase):
+    """
+    Test class for the Cinema model.
+    """
+    def test_cinema_creation(self):
+        """
+        Test case for creating a Cinema object.
+        """
+        cinema = Cinema.objects.create(name='Cinema Test', city='City Test')
+        self.assertEqual(cinema.name, 'Cinema Test')
+        self.assertEqual(cinema.city, 'City Test')
+
+
+class MovieModelTest(TestCase):
+    """
+    Test class for the Movie model.
+    """
+    def test_movie_creation(self):
+        """
+        Test case for creating a Movie object.
+        """
+        movie = Movie.objects.create(title='Movie Test', category='Action', description='Description Test', image_url='www.test.com')
+        self.assertEqual(movie.title, 'Movie Test')
+        self.assertEqual(movie.category, 'Action')
+        self.assertEqual(movie.description, 'Description Test')
+        self.assertEqual(movie.image_url, 'www.test.com')
+
+
+class ShowModelTest(TestCase):
+    """
+    Test class for the Show model.
+    """
+    def test_show_creation(self):
+        """
+        Test case for creating a Show object.
+        """
+        cinema = Cinema.objects.create(name='Cinema Test', city='City Test')
+        movie = Movie.objects.create(title='Movie Test', category='Action', description='Description Test', image_url='www.test.com')
+        show = Show.objects.create(cinema=cinema, movie=movie, date='2023-06-01', time='13:00:00', booking_link='www.test.com')
+        self.assertEqual(show.cinema, cinema)
+        self.assertEqual(show.movie, movie)
+        self.assertEqual(show.date, '2023-06-01')
+        self.assertEqual(show.time, '13:00:00')
+        self.assertEqual(show.booking_link, 'www.test.com')
