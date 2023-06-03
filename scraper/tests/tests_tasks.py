7eace3138c6f8165d15c4737a627b9b0cd6621e9
Marek <marekdrag@gmail.com>
2023-06-04 00:59:17 +0200

    Added unit tests for tasks.py
    This commit includes the following changes:
    
    - Added unit tests for the add_days function to ensure the correct date calculation.
    - Added unit tests for the BaseScrapeStore abstract class to ensure it raises NotImplementedError when not overridden.
    - Added unit tests for the MultikinoScrapeStore and HeliosScrapeStore classes, covering the creation of scraper objects, date generation, and cinema creation.
    - Added unit tests for Celery tasks, checking if the scrape_and_store methods are called correctly.
    
    These unit tests will help to ensure the correctness of the code in tasks.py as changes are made in the future.

---
+++ scraper/tests/tests_tasks.py
@@ -0,0 +1,132 @@
+from datetime import datetime
+from unittest import TestCase
+from unittest.mock import patch, Mock
+from scraper.tasks import add_days, BaseScrapeStore
+from scraper.tasks import MultikinoScrapeStore, HeliosScrapeStore, scrape_and_store_multikino, scrape_and_store_helios
+from scraper.scraper import MultikinoScraper, HeliosScraper
+
+
+class TestAddDays(TestCase):
+    """
+    Test class for the add_days function.
+    """
+
+    def test_add_days(self):
+        """
+        Test case to check the add_days function.
+        """
+        today = datetime(2022, 10, 1)
+        result = add_days(today, 4)
+        self.assertEqual(result, datetime(2022, 10, 5))
+
+
+class TestBaseMovieScraper(TestCase):
+    """
+    Test class for the BaseScrapeStore class.
+    """
+
+    def test_get_movie_info_not_implemented(self):
+        """
+        Test case to check that get_movie_info raises NotImplementedError.
+        """
+        class FakeMovieScraper(BaseScrapeStore):
+            pass
+
+        with self.assertRaises(TypeError):
+            FakeMovieScraper(["city"], "date").create_scraper()
+            FakeMovieScraper(["city"], "date").get_dates()
+            FakeMovieScraper(["city"], "date").scrape_and_store_data()
+
+
+class TestMultikinoScrapeStore(TestCase):
+    """
+    Test class for the MultikinoScrapeStore class.
+    """
+
+    @patch('scraper.tasks.MultikinoScraper')
+    def setUp(self, MockScraper):
+        self.mock_scraper = MockScraper()
+        self.multikino = MultikinoScrapeStore(["Kraków"])
+
+    def test_create_scraper(self):
+        """
+        Test case to check the create_scraper method.
+        """
+        self.assertIsInstance(self.multikino.create_scraper(), MultikinoScraper)
+
+    def test_get_dates(self):
+        """
+        Test case to check the get_dates method.
+        """
+        dates = self.multikino.get_dates()
+        self.assertEqual(len(dates), self.multikino.AMOUNT_OF_DAYS)
+        self.assertIsInstance(dates[0], str)
+
+    def test_create_cinema(self):
+        """
+        Test case to check the create_cinema method.
+        """
+        with patch('scraper.tasks.Cinema.objects.get_or_create', return_value=(Mock(), True)) as mock_get_or_create:
+            self.multikino.create_cinema('Kraków')
+        mock_get_or_create.assert_called_with(
+            name='multikino',
+            city='Kraków'
+        )
+
+
+class TestHeliosScrapeStore(TestCase):
+    """
+    Test class for the HeliosScrapeStore class.
+    """
+
+    @patch('scraper.tasks.HeliosScraper')
+    def setUp(self, MockScraper):
+        self.mock_scraper = MockScraper()
+        self.helios = HeliosScrapeStore({"Kraków": 1})
+
+    def test_create_scraper(self):
+        """
+        Test case to check the create_scraper method.
+        """
+        self.assertIsInstance(self.helios.create_scraper(), HeliosScraper)
+
+    def test_get_dates(self):
+        """
+        Test case to check the get_dates method.
+        """
+        dates = self.helios.get_dates()
+        self.assertEqual(len(dates), self.helios.AMOUNT_OF_DAYS)
+        self.assertIsInstance(dates[0], str)
+
+    def test_create_cinema(self):
+        """
+        Test case to check the create_cinema method.
+        """
+        with patch('scraper.tasks.Cinema.objects.get_or_create', return_value=(Mock(), True)) as mock_get_or_create:
+            self.helios.create_cinema('Kraków')
+        mock_get_or_create.assert_called_with(
+            name='helios',
+            city='Kraków'
+        )
+
+
+class TestCeleryTasks(TestCase):
+    """
+    Test class for the Celery tasks.
+    """
+
+    @patch('scraper.tasks.MultikinoScrapeStore.scrape_and_store_data')
+    def test_scrape_and_store_multikino(self, mock_scrape_and_store_data):
+        """
+        Test case to check the scrape_and_store_multikino task.
+        """
+        scrape_and_store_multikino(["Kraków"])
+        mock_scrape_and_store_data.assert_called_once()
+
+    @patch('scraper.tasks.HeliosScrapeStore.scrape_and_store_data')
+    def test_scrape_and_store_helios(self, mock_scrape_and_store_data):
+        """
+        Test case to check the scrape_and_store_helios task.
+        """
+        scrape_and_store_helios({"Kraków": 1})
+        mock_scrape_and_store_data.assert_called_once()
