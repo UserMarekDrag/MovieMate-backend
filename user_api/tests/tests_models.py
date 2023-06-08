e0727e2d6bf39540a0c916b7d6358d5d98e6f2ff
Marek <marekdrag@gmail.com>
2023-06-08 16:13:27 +0200

    Added unit tests for models.py in user_api APP
    This commit includes the addition of new unit tests for the models in the `models.py` file of the `user_api` application. It specifically adds tests for the creation of regular and superuser `AppUser` instances, along with verifying the required fields for user creation.
    
    In addition, a new `__init__.py` file has been created in the `tests` folder of the `user_api` application to ensure proper recognition of the tests by the testing framework.
    
    Here's a brief overview of the new tests:
    
    - `UserManagerTestCase`: Tests the user management features, including creation of regular and super users, and checks the required fields for user creation.

---
+++ user_api/tests/tests_models.py
@@ -0,0 +1,41 @@
+from django.test import TestCase
+from user_api.models import AppUser
+
+
+class UserManagerTestCase(TestCase):
+    def setUp(self):
+        """
+        Set up the test by creating user instances.
+        """
+        self.user1 = AppUser.objects.create_user(
+            email='testuser1@example.com',
+            username='testuser1',
+            password='password123'
+        )
+        self.superuser = AppUser.objects.create_superuser(
+            email='admin@example.com',
+            username='admin',
+            password='adminpassword'
+        )
+
+    def test_create_user(self):
+        """
+        Test the creation of a regular user.
+        """
+        self.assertEqual(self.user1.email, 'testuser1@example.com')
+        self.assertEqual(self.user1.username, 'testuser1')
+        self.assertFalse(self.user1.is_superuser)
+
+    def test_create_superuser(self):
+        """
+        Test the creation of a superuser.
+        """
+        self.assertEqual(self.superuser.email, 'admin@example.com')
+        self.assertEqual(self.superuser.username, 'admin')
+        self.assertTrue(self.superuser.is_superuser)
+
+    def test_required_fields(self):
+        """
+        Test the required fields for user creation.
+        """
+        self.assertEqual(AppUser.REQUIRED_FIELDS, ['username'])
