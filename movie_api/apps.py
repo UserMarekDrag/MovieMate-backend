53cf1a9611352efac82b4eac00df3ad665017c35
Marek <marekdrag@gmail.com>
2023-06-14 12:30:58 +0200

    Added docstring to UserApiConfig and correct docstring typo in movie_api
    This commit introduces two main improvements:
    
    1. Added a class docstring to the UserApiConfig in the `apps.py` of the `user_api` application. This docstring provides a clear understanding of the class's purpose and functionality, enhancing the maintainability and readability of the code.
    
    2. Corrected a typographical error in the docstring within `apps.py` of the `movie_api` application. This correction ensures the accuracy and professionalism of our code documentation.
    
    These updates contribute towards maintaining a high standard of code quality and readability.

--- movie_api/apps.py
+++ movie_api/apps.py
@@ -3,9 +3,9 @@ from django.apps import AppConfig
 
 class MovieApiConfig(AppConfig):
     """
-    Configuration for the Scraper application.
+    Configuration for the Movie Api application.
 
-    This class is used by Django's application registry to configure the Movie_api app.
+    This class is used by Django's application registry to configure the movie_api app.
     It contains metadata about the app, like its name and the default type of auto-created primary key field.
     """
     default_auto_field = 'django.db.models.BigAutoField'
--- user_api/apps.py
+++ user_api/apps.py
@@ -2,5 +2,11 @@ from django.apps import AppConfig
 
 
 class UserApiConfig(AppConfig):
+    """
+    Configuration for the User Api application.
+
+    This class is used by Django's application registry to configure the user_api app.
+    It contains metadata about the app, like its name and the default type of auto-created primary key field.
+    """
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'user_api'
