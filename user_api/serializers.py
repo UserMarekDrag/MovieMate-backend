2c0d212c4f02be510f795e2161168762846e58d9
Marek <marekdrag@gmail.com>
2023-06-14 12:32:35 +0200

    Updated user_api to comply with Pylint 10/10 score
    This commit addresses several Pylint warnings and errors in the `user_api` application, significantly improving the code quality and compliance with PEP8.
    
    Key updates include:
    
    1. In `views.py`, provided docstrings for classes and methods and made sure all paths in the `UserLogin` class's `post` method return a response.
    
    2. In `serializers.py`, added missing docstrings and replaced unnecessary `pass` statements in the `UserLoginSerializer` class with `NotImplementedError` exceptions.
    
    By implementing these changes, the Pylint score for the `user_api` application has been improved to a perfect 10/10.

--- user_api/serializers.py
+++ user_api/serializers.py
@@ -1,6 +1,5 @@
-from rest_framework import serializers
+from rest_framework import serializers, exceptions
 from django.contrib.auth import get_user_model, authenticate
-from rest_framework.exceptions import ValidationError
 from .validations import custom_validation
 
 
@@ -8,15 +7,24 @@ UserModel = get_user_model()
 
 
 class UserRegisterSerializer(serializers.ModelSerializer):
+    """
+    Serializer for user registration.
+    """
     class Meta:
         model = UserModel
         fields = '__all__'
 
     def validate(self, data):
+        """
+        Validate user registration data.
+        """
         custom_validation(data)
         return data
 
     def create(self, validated_data):
+        """
+        Create a new user instance.
+        """
         user_obj = UserModel.objects.create_user(
             email=validated_data['email'],
             username=validated_data['username'],
@@ -26,17 +34,38 @@ class UserRegisterSerializer(serializers.ModelSerializer):
 
 
 class UserLoginSerializer(serializers.Serializer):
+    """
+    Serializer for user login.
+    """
     email = serializers.EmailField()
     password = serializers.CharField()
 
+    def create(self, validated_data):
+        """
+        Not used, included for completeness of Serializer implementation.
+        """
+        raise NotImplementedError("Method 'create' not implemented.")
+
+    def update(self, instance, validated_data):
+        """
+        Not used, included for completeness of Serializer implementation.
+        """
+        raise NotImplementedError("Method 'update' not implemented.")
+
     def check_user(self, clean_data):
+        """
+        Check user with given credentials.
+        """
         user = authenticate(username=clean_data['email'], password=clean_data['password'])
         if not user:
-            raise ValidationError('user not found')
+            raise exceptions.ValidationError('user not found')
         return user
 
 
 class UserSerializer(serializers.ModelSerializer):
+    """
+    Serializer for User instances.
+    """
     class Meta:
         model = UserModel
         fields = ('email', 'username')
--- user_api/views.py
+++ user_api/views.py
@@ -1,16 +1,23 @@
-from django.contrib.auth import get_user_model, login, logout
+from django.contrib.auth import login, logout
 from rest_framework.authentication import SessionAuthentication
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from .serializers import UserRegisterSerializer, UserLoginSerializer, UserSerializer
 from rest_framework import permissions, status
+
+from .serializers import UserRegisterSerializer, UserLoginSerializer, UserSerializer
 from .validations import custom_validation, validate_email, validate_password
 
 
 class UserRegister(APIView):
+    """
+    Class based view for user registration.
+    """
     permission_classes = (permissions.AllowAny, )
 
     def post(self, request):
+        """
+        Handles user registration POST request.
+        """
         clean_data = custom_validation(request.data)
         serializer = UserRegisterSerializer(data=clean_data)
         if serializer.is_valid(raise_exception=True):
@@ -21,10 +28,16 @@ class UserRegister(APIView):
 
 
 class UserLogin(APIView):
+    """
+    Class based view for user login.
+    """
     permission_classes = (permissions.AllowAny,)
     authentication_classes = (SessionAuthentication,)
 
     def post(self, request):
+        """
+        Handles user login POST request.
+        """
         data = request.data
         assert validate_email(data)
         assert validate_password(data)
@@ -33,21 +46,34 @@ class UserLogin(APIView):
             user = serializer.check_user(data)
             login(request, user)
             return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(status=status.HTTP_400_BAD_REQUEST)
 
 
 class UserLogout(APIView):
+    """
+    Class based view for user logout.
+    """
     permission_classes = (permissions.AllowAny,)
     authentication_classes = ()
 
     def post(self, request):
+        """
+        Handles user logout POST request.
+        """
         logout(request)
         return Response(status=status.HTTP_200_OK)
 
 
 class UserView(APIView):
+    """
+    Class based view for retrieving user information.
+    """
     permission_classes = (permissions.IsAuthenticated,)
     authentication_classes = (SessionAuthentication,)
 
     def get(self, request):
+        """
+        Handles GET request to retrieve user information.
+        """
         serializer = UserSerializer(request.user)
         return Response({'user': serializer.data}, status=status.HTTP_200_OK)
