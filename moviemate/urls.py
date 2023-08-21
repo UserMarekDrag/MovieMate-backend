40417a79b6b558c3f09f378e2a2bdaf0e92ec2a9
Marek Drąg <102695033+UserMarekDrag@users.noreply.github.com>
2023-08-21 15:32:35 +0200

    Integrated quality check workflow GitHub Actions
    * Introduced a quality check workflow using GitHub Actions.
    * Made several fixes and adjustments in the workflow file to ensure it runs smoothly.
    * Adjusted `.pylintrc` settings to ignore C0103 warning in `urls.py`.

--- moviemate/urls.py
+++ moviemate/urls.py
@@ -20,6 +20,7 @@ from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
+# pylint: disable=C0103
 schema_view = get_schema_view(
     openapi.Info(
         title="MovieMate API",
@@ -29,6 +30,7 @@ schema_view = get_schema_view(
     public=True,
     permission_classes=(permissions.AllowAny,),
 )
+#
 
 urlpatterns = [
     path('admin/', admin.site.urls),
