eec3e8fce0dcf7bc4fa9dd97088fadb4a79d5c9a
Marek <marekdrag@gmail.com>
2023-10-15 23:33:10 +0200

    Add email confirmation functionality during user registration

--- mailer/views.py
+++ mailer/views.py
@@ -1,12 +1,25 @@
 from django.core.mail import send_mail
-from django.http import HttpResponse
 
 
-def send_test_email(request):
-    """Send an email on verify address email"""
-    send_mail(
-        'Test Subject',
-        'Test message body.',
-        'moviemate.md@gmail.com',
-        ['marekdrag@gmail.com']
-    )
+def send_verification_email(user, verification_url):
+    """
+    Send an email to the user containing the verification link.
+    Args:
+        user (AppUser): User instance to whom the email should be sent.
+        verification_url (str): Verification link for the user.
+    Returns:
+        int: Number of successfully sent emails.
+    """
+    email_subject = "Registration Confirmation"
+    email_body = f"""
+    Hello {user.username},
+
+    Thank you for registering with us. Please click the link below to activate your account:
+    {verification_url}
+
+    If you did not initiate this request, kindly ignore this email.
+
+    Best regards,
+    MovieMate
+    """
+    send_mail(email_subject, email_body, 'moviemate.md@gmail.com', [user.email])
--- user_api/models.py
+++ user_api/models.py
@@ -1,6 +1,9 @@
+import uuid
+
 from django.db import models
 from django.contrib.auth.base_user import BaseUserManager
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.utils import timezone
 
 
 class AppUserManager(BaseUserManager):
@@ -34,7 +37,7 @@ class AppUserManager(BaseUserManager):
             raise ValueError('A username is required.')
         if not password:
             raise ValueError('A password is required.')
-        user = self.create_user(email, username, password, is_staff=True, is_superuser=True)
+        user = self.create_user(email, username, password, is_staff=True, is_superuser=True, is_active=True)
         user.save()
         return user
 
@@ -47,9 +50,34 @@ class AppUser(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(max_length=50, unique=True)
     username = models.CharField(max_length=50)
     is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=False)
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username']
     objects = AppUserManager()
 
     def __str__(self):
         return self.username
+
+
+class EmailVerification(models.Model):
+    """
+    Model to store the verification tokens for email confirmation of users.
+    """
+    user = models.OneToOneField(AppUser, on_delete=models.CASCADE)
+    token = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+    expiry_date = models.DateTimeField()
+
+    @classmethod
+    def generate_token_for_user(cls, user):
+        """
+        Generate a unique token for user email verification.
+        Args:
+            user (AppUser): User instance for whom the token is to be generated.
+        Returns:
+            uuid.UUID: Generated unique token.
+        """
+        token = uuid.uuid4()
+        expiry_date = timezone.now() + timezone.timedelta(days=1)
+        verification_token = cls(user=user, token=token, expiry_date=expiry_date)
+        verification_token.save()
+        return token
