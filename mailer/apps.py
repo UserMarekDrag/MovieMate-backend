812ba276caac1b1cd2dfc113debb3249a6b4b967
Marek <marekdrag@gmail.com>
2023-10-15 23:32:33 +0200

    Introduce enhancements and clean-up in the mailer app

--- mailer/apps.py
+++ mailer/apps.py
@@ -2,5 +2,11 @@ from django.apps import AppConfig
 
 
 class MailerConfig(AppConfig):
+    """
+    Configuration for the Mailer application.
+
+    This class is used by Django's application registry to configure the mailer app.
+    It contains metadata about the app, like its name and the default type of auto-created primary key field.
+    """
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'mailer'
--- mailer/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('send-test-email/', views.send_test_email, name='send_test_email'),
-]
